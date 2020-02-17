aab79ee375a89e23122c0c52b850e4d8c5a7e3e6
pedrotiv <pedrotiva@gmail.com>
2020-02-17 11:24:54 -0300

    feat chap_4to6: init

---
+++ chap_4to6/chap_4to6.py
@@ -0,0 +1,4 @@
+import music21 as m21 
+# The Stream object and its subclasses (Score, Part, Measure) are the fundamental containers 
+# for music21 objects such as Note, Chord, Clef, TimeSignature objects.
+# A container is like a Python list.
