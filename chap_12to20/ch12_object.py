b6b89dedb1d6b0f52ab41e5366eee3933f4dbbd9
edu4u-games <59312731+edu4u-games@users.noreply.github.com>
2020-02-22 12:23:21 -0300

    :construction: ch12_object

--- chap_12to20/ch12_object.py
+++ chap_12to20/ch12_object.py
@@ -1,9 +1,22 @@
 import music21 as m 
 
-n= m.note.Note('A-2', type= 'half')
-s= m.stream.Stream()
-s.insert(0, n)
-# s.show('text')
-
 # Note objects, Chord objects, Stream objects, etc., are subclasses of Music21Object.
+# Streams have many ways of filtering out Music21Objects (a.k.a. “elements”) according to class.
+# The easiest way is with .getElementsByClass:
+s= m.stream.Stream()
+n= m.note.Note('A-4')
+n.quarterLength = 4
+r= m.note.Rest()
+s.append(m.clef.TrebleClef())
+s.append(m.meter.TimeSignature('3/4'))
+s.append(m.note.Note("A"))
+s.append(m.note.Rest())
+s.append(m.note.Note("B"))
+s.append(r)
+s.append(m.note.Note('B-3'))
+s.append(n)
+for element in s.getElementsByClass('Note'):
+    print(element)
+s.show('text')
+s.show()
 
