5b04ea7ed2a4139cb312141aab373fe980701b53
edu4u-games <59312731+edu4u-games@users.noreply.github.com>
2020-03-05 10:11:56 -0300

    :feat: ch15: end this chapter

--- chap_12to20/ch15_keys.py
+++ chap_12to20/ch15_keys.py
@@ -79,7 +79,7 @@ print('Some other final keys that the Bach piece could have been in: \n', fis.al
 c = bach.measures(1, 4).chordify()
 for ch in c.recurse().getElementsByClass('Chord'):
     ch.closedPosition(inPlace=True, forceOctave=4)
-c.show()
+# c.show()
 # So, how does it know what the key is? The key analysis routines are a variation of the famous algorithm
 #  developed by Carol Krumhansl and Mark A. Schmuckler called probe-tone key finding. (used above)
 # The distribution of pitches used in the piece are compared to sample distributions of pitches 
@@ -95,8 +95,8 @@ print('Eb key, its classes: ', k.classes)
 print('The 2nd degree of Eb scale is ',k.pitchFromDegree(2))
 # (octaves in 4 and 5 are chosen just to give some ordering to the pitches)
 
-#############    Key Context and Note Spelling   ##################
 
+#############    Key Context and Note Spelling   ##################
 # Key and KeySignature objects affect how notes are spelled in some situations. 
 # Let’s set up a simple situation of a F-natural whole note in D major and then B-flat minor.
 s = m21.stream.Stream()
@@ -105,9 +105,63 @@ s.append(m21.note.Note('F', type='whole'))
 s.append(m21.key.Key('b-', 'minor'))
 s.append(m21.note.Note('F', type='whole'))
 # s2 = s.makeNotation()
-s.show()
+# s.show()
 # When we transpose each note up a half step (n.transpose(1)), music21 understands that the first F-natural
-#  should become F-sharp, while the second one will fit better as a G-flat.
+#  should become F-sharp, while the second one will fit better as a G-flat. (see below)
 for n in s.recurse().notes:
     n.transpose(1, inPlace=True)
-s.show()
+# s.show()
+
+# Exercise in all major keys, ascending by step in playing or singing thirds.
+pitchStream = m21.stream.Part()
+pitchStream.insert(0, m21.meter.TimeSignature('4/4'))
+for step in ('c', 'e', 'd', 'f', 'e', 'g', 'f', 'a', 'g', 'e', 'f', 'd', 'c', 'e', 'c'):
+    n = m21.note.Note(step, type='eighth')
+    n.pitch.octave = 4
+    pitchStream.append(n)
+pitchStream.notes[-1].duration.type = 'quarter'
+pitchStream.makeMeasures(inPlace=True)
+# pitchStream.show()
+# This melody does not have a key associated with it. Let’s put a Key of C Major at the beginning of the piece:
+k = m21.key.Key('C')
+pitchStream.measure(1).insert(0, k)
+# pitchStream.show()
+# Note that putting the key of C into the Stream doesn’t change what it looks like when we show the Stream, 
+# since there are no sharps or flats. But what makes the difference between an instrumental and
+#  a vocal exercise is the act of transposition. When we transpose the Key object up 1 semitone, 
+# to D-flat major, it will show up:
+k.transpose(1, inPlace=True)
+# pitchStream.show()
+# Now the key signature is D-flat, but the notes are still in C-major, so we should transpose them also:
+for n in pitchStream.recurse().notes:
+    n.transpose(1, inPlace=True)
+# pitchStream.show()
+# Notice that we choose a semitone transposition and not a diatonic transposition such as minor second ("m2");
+#  minor second would work just as good in this case, but then to do another half-step up, 
+# we would need to remember to transpose by an augmented unison ("A1") so that D-flat became D-natural
+#  and not E-double-flat. The semitone transposition is smart enough to make sure that the Key object 
+# remains between six-flats and six-sharps. 
+# Not only that, but the notes will match the best spelling for the current key signature.
+k.transpose(1, inPlace=True)
+for n in pitchStream.recurse().notes:
+    n.transpose(1, inPlace=True)
+# pitchStream.show()
+# and again:
+k.transpose(1, inPlace=True)
+for n in pitchStream.recurse().notes:
+    n.transpose(1, inPlace=True)
+# pitchStream.show()
+# So, we can make a nice, ascending vocal exercise by varying the transposition amount from 0 to 7 
+# (or however high you can sing) and putting each of the two-measure excerpts together into one Part.
+# We will introduce the tinyNotation format here, which will be described in the next chapter:
+out = m21.stream.Part()
+for i in range(0, 8):
+    pitchStream = m21.converter.parse("tinyNotation: 4/4 c8 e d f e g f a g e f d c e c4")
+    k = m21.key.Key('C')
+    pitchStream.measure(1).insert(0, k)
+    k.transpose(i, inPlace=True)
+    for n in pitchStream.recurse().notes:
+        n.transpose(i, inPlace=True)
+    for m in pitchStream:
+        out.append(m)
+out.show()
