54fb4b607d71313daaef7c5ef0a8e33f09b33f7a
edu4u-games <59312731+edu4u-games@users.noreply.github.com>
2020-03-06 12:56:44 -0300

    :init: chap16_tiny: init ch16.py

---
+++ chap16_tiny/ch16.py
@@ -0,0 +1,114 @@
+import music21 as m21
+
+# The basic:
+# If the number for a duration is omitted, then the next note uses the previous note’s duration:
+s = m21.converter.parse('tinyNotation: 4/4 C4 D E F G A B c')
+# s.show()
+
+# Case determines octave: “C” = the c in bass clef (C3) while “c” = middle C (C4). 
+s = m21.converter.parse("tinyNotation: 4/4 CC4 C4 c2")
+# s.show()
+
+# Periods signify dots, “r” is for a rest, and “~” indicates a tie:
+s = m21.converter.parse('tinyNotation: 4/4 C4 D E8 F G16 A B c C.4 D8~ D8 r c4')
+# s.show()
+
+# Sharps, flats, and, if desired for clarity, naturals are indicated with #, - (not b) and, n, respectively:
+s = m21.converter.parse('tinyNotation: 4/4 c4 c# c c## cn c- c-- c c1')
+# s.show()
+
+# A lyric syllable is specified by appending it after the note with an underscore:
+s = m21.converter.parse('tinyNotation: 4/4 c4_Ale- d2_lu- e4_ia')
+# s.show()
+
+# triplets are possible by enclosing the triplet notes in curly brackets along with a special trip prefix:
+s = m21.converter.parse('tinyNotation: 4/4 c4 trip{c8 d e} trip{f4 g a} b-1')
+# s.show()
+
+# Applying an id to a note with the “=” tag, and then make changes to it using music21:
+s = m21.converter.parse('tinyNotation: 4/4 c4 d=id2 e f')
+n = s.recurse().getElementById('id2')
+ch = m21.chord.Chord('D4 F#4 A4')
+ch.style.color = 'blue'
+n.activeSite.replace(n, ch)
+# s.show()
+
+# And that’s how we use TinyNotation, about 90% of the time. 
+# But when I need to, I can make something more complex we can expanding 
+# TinyNotation through new tokens (like the Notes, Rests, and TimeSignatures), 
+# modifiers (such as the = for assigning .id, or the _ for assigning a lyric) and 
+# states (such as the triplet state enclosed in curly brackets).
+
+# The first thing that we’ll need to know in order to expand TinyNotation is how to get 
+# at the TinyNotation Converter itself (which is different from the basic converter.parse() call). 
+# It’s found in the tinyNotation module, and is called with a set of music to parse.
+tnc = m21.tinyNotation.Converter('6/8 e4. d8 c# d e2.')
+# We run the converter by calling .parse() on it and then there will be a 
+# Stream (generally a stream.Part object) in the .stream attribute of the Converter.
+tnc.parse()
+s = tnc.stream
+# s.show()
+
+######################    Beyond the tiny notation    ###################
+
+######################    Adding new tokens           ###################
+
+# TinyNotation does not have a way of specifying the Key (and thereby the KeySignature) of a piece or region.
+#  Let’s add that “one more thing” with a new Token:
+class KeyToken(m21.tinyNotation.Token):
+    def parse(self, parent):
+        keyName = self.token
+        return m21.key.Key(keyName)
+# The KeyToken is a subclass of the tinyNotation.Token class. When it is parsed, .parse() is 
+# called and it is passed a reference to the Converter object, and important information is stored 
+# in the self.token attribute. The Converter calling parse() expects that a Music21Object or None will
+# be returned.Here we’re going to return a Key object.
+# Now that we’ve defined this particular Token, we’ll need to teach the Converter when to call it.
+# We’ll say that any token (that is, data separated by spaces) which begins with a k is a new Key object,
+# and that the relevant data is everything after the k. And we’ll add this to a blank Converter object.
+tnc = m21.tinyNotation.Converter()
+keyMapping = (r'k(.*)', KeyToken)
+tnc.tokenMap.append(keyMapping)
+# Next, let’s create a fragment of TinyNotation to see if this works, using the load() method:
+tnc.load('4/4 kE- G4 A G B kf# A1')
+tnc.parse()
+s = tnc.stream
+# s.show()
+
+# Now that we have a way to create totally new Tokens, let’s look at ways we can modify existing tokens. 
+# Let’s first create a simple Modifier that changes the color of individual notes after they’ve been parsed:
+class ColorModifier(m21.tinyNotation.Modifier):
+    def postParse(self, m21Obj):
+        m21Obj.style.color = self.modifierData
+        return m21Obj
+# Now we’ll modify our Converter object to make it so that the ColorModifier is run anytime that a color
+#  name is put in angle brackets after a Token, and then test it on a simple stream.
+tnc.modifierAngle = ColorModifier
+tnc.load('3/4 c4<red> d<green> e-<blue>')
+tnc.parse()
+# tnc.stream.show()
+
+# Here’s a less silly modifier which replaces the Note object that comes in with a ChordSymbol object 
+# that combines the root name from the Note with the data from the modifier:
+class HarmonyModifier(m21.tinyNotation.Modifier):
+    def postParse(self, n):
+        cs = m21.harmony.ChordSymbol(n.pitch.name + self.modifierData)
+        cs.duration = n.duration
+        return cs
+
+tnc.modifierUnderscore = HarmonyModifier
+tnc.load('4/4 C2_maj7 D4_m E-_sus4')
+tnc.parse().stream.show('t')
+
+# If we leave in the bass note and instead add the ChordSymbol to the stream, 
+# then we’ll be able to see it on the score:
+class HarmonyModifier(m21.tinyNotation.Modifier):
+    def postParse(self, n):
+        cs = m21.harmony.ChordSymbol(n.pitch.name + self.modifierData)
+        self.parent().stream.append(cs)
+        return n
+
+tnc.modifierUnderscore = HarmonyModifier
+tnc.load('4/4 C2_maj7 D4_m E-_sus4')
+tnc.parse().stream.show()
+tnc.parse().stream.show('t')
