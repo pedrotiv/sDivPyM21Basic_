69d293b02ff561cba4247c6e335fdb85dc542efd
edu4u-games <59312731+edu4u-games@users.noreply.github.com>
2020-03-09 18:06:42 -0300

    add files of idea project and code to hrm_k618.py

--- std_hrm_k618_ave_verum/hrm_k618.py
+++ std_hrm_k618_ave_verum/hrm_k618.py
@@ -1,25 +1,33 @@
 import music21 as m21 
 
+'''
+Studies of Mozart Ave Verum K618
+Next:   We find each modulations and we adjust the rn variable to its
+        Delete unused lines.
+'''
+
 b = m21.corpus.parse('k618')
 # b.show()
 b_chords = b.chordify()
 # b_chords.show()
-# Adding b_chords in b and showing two measures. (0 is a pickup measure)
-b.insert(0, b_chords)
+# Adding b_chords in b. (0 is a pickup measure)
 # b.show()
 # let’s put all these chords in closedPosition
 for c in b_chords.recurse().getElementsByClass('Chord'):
     # c.closedPosition(forceOctave=4, inPlace=True)
-    c.annotateIntervals()    
+    # c.annotateIntervals()    
+    pass
 # b.show()
-b_chords.show()
+# b_chords.show()
 # We can use the function roman.romanNumeralFromChord to label each of the chordified Chords:
 for c in b_chords.recurse().getElementsByClass('Chord'):
-    rn = m21.roman.romanNumeralFromChord(c, m21.key.Key('A'))
+    rn = m21.roman.romanNumeralFromChord(c, m21.key.Key('D'))
     c.addLyric(str(rn.figure))
 # We can also just extract the lyrics, where we stored the RomanNumeral information:
-for c in b_chords.measures(0,2).flat:
-    if 'Chord' not in c.classes:
-        continue
-    print(c.lyric, end=' ')
+# for c in b_chords.measures(0,2).flat:
+#     if 'Chord' not in c.classes:
+#         continue
+#     print(c.lyric, end=' ')
+b.insert(0, b_chords)
+b.show()
 
